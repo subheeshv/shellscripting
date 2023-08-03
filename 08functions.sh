007cbf58e4644a32e5e9724f216b9adb72f702e8
subhe@DESKTOP-OKROP93 <subheeshv@gmail.com>
2023-08-03 23:20:19 +0530

    Autopush

--- 08functions.sh
+++ 08functions.sh
@@ -6,3 +6,11 @@ hai() {
     echo hai function compldted
 }
 hai
+
+stat() {
+    echo "number of sessions opened are $(who| wc -l)"
+    echo "Todays date is $(date +%F)"
+    echo "average CPU utilisation in last fibe minutes $(uptime | awk -F : '{Print $NF}' | awk -F , '{Print $2}')"
+}
+
+stat
