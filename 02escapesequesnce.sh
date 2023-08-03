e87c55083bcaf835d1848afa0f2440320280a3ba
subhe@DESKTOP-OKROP93 <subheeshv@gmail.com>
2023-08-03 18:57:23 +0530

    auto push

--- 02escapesequesnce.sh
+++ 02escapesequesnce.sh
@@ -2,3 +2,6 @@
 #\n indicates new line
 #-e used to enable escape sequence
 echo -e "line1\nline2"
+
+echo -e "cloud\tdevops\tare\tbooming"
+echo -e "We\tare\tlearning\tcloud devops \nThis\tis\tmy\tworkstation"
