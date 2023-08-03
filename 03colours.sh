8f3d2a06a6208dfdea94d2909e9e6b954d3d3a36
subhe@DESKTOP-OKROP93 <subheeshv@gmail.com>
2023-08-03 20:08:30 +0530

    Autopush

--- 03colours.sh
+++ 03colours.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo -e "\e[33m my message printed in clour \e[0m"
-echo -e "\e[36m;33m my message and background printed in colours \e[0m"
+echo -e "\e[36;33m my message and background printed in colours \e[0m"
