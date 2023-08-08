6c210aee94a980dabe722f4b9f9ec340442476e8
subhe@DESKTOP-OKROP93 <subheeshv@gmail.com>
2023-08-08 23:38:24 +0530

    Auto-push

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -e
-
 USER_ID=$(id -u)
+COMPONENT=frontend
+LOGFILE="/tmp/$COMPONENT.log"
 
 if [ $USERID -ne 0 ];then
     echo -e "\e[31m Script is expected to execute with sudo user \e[0m \n \t example:sudo bash wrapper.sh frontend"
@@ -17,41 +17,43 @@ stat() {
     fi
 }
 
-echo -e "\e[35m Configuring frontend.....! \e[0m \n"
+echo -e "\e[35m Configuring ${COMPONENT}.....! \e[0m \n"
 
-echo -n "installing frontend :"
-yum install nginx -y &>> /tmp/frontend.log
+echo -n "installing ${COMPONENT} :"
+yum install nginx -y &>> ${LOGFILE}
 stat $?
 
 
 echo -n "Starting Nginx:" 
-systemctl enable nginx   &>>  /tmp/frontend.log 
-systemctl start nginx    &>>  /tmp/frontend.log 
+systemctl enable nginx   &>>  ${LOGFILE}
+systemctl start nginx    &>>  ${LOGFILE}
 stat $?
 
-echo -n "Downloading the frontend component:"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" 
+echo -n "Downloading the ${COMPONENT} component:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
 stat $? 
 
 
-echo -n "Clean up of frontend : "
+echo -n "Clean up of ${COMPONENT} : "
 cd /usr/share/nginx/html    
-rm -rf *     &>>  /tmp/frontend.log
+rm -rf *     &>>  ${LOGFILE}
 stat $?
 
-echo -n "Extracting Frontend :"
+echo -n "Extracting ${COMPONENT} :"
 unzip /tmp/frontend.zip     &>>  /tmp/frontend.log
 stat $?
 
-echo -n "Sorting the frontend files :"
+echo -n "Sorting the ${COMPONENT} files :"
 mv frontend-main/* .
 mv static/* .
-rm -rf frontend-main README.md  &>>  /tmp/frontend.log
+rm -rf frontend-main README.md  &>>  ${LOGFILE}
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $? 
 
 
-echo -n "Restarting Frontend:"
-systemctl daemon-reload     &>>  /tmp/frontend.log
-systemctl restart nginx     &>>  /tmp/frontend.log
+echo -n "Restarting ${COMPONENT}:"
+systemctl daemon-reload     &>>  ${LOGFILE}
+systemctl restart nginx     &>>  ${LOGFILE}
 stat $?
+
+echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
