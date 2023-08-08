b528166813e0f888a489402997a9bed8f10216fc
subhe@DESKTOP-OKROP93 <subheeshv@gmail.com>
2023-08-09 00:02:47 +0530

    Auto-push

---
+++ roboshop/Components/mongodb.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+COMPONENT=mongodb
+LOGFILE="/tmp/${COMPONENT}.log"
+
+if [$USER_ID -ne 0] ; then
+    echo -e "e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
+    exit 1
+fi
+
+stat() {
+    if [$1 -eq 0] ; then
+        echo -e "\e[32m success \e[0m"
+    else
+        echo -e "\e[31m failure \e[0m"
+    exit 2
+    fi
+}
+
+echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
+
+echo -n "Configuring ${COMPONENT} repo :"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo 
+stat $? 
+
+echo -n "installing ${COMPONENT} :"
+yum install mongodb-org -y &>> ${LOGFILE}
+stat $?
+
+
