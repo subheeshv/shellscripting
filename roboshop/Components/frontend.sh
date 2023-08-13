29955606333e04e8c16bd3ee7460f5baac0e3d8b
subhe@DESKTOP-OKROP93 <subheeshv@gmail.com>
2023-08-14 00:17:49 +0530

    Auto-push

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -27,3 +27,37 @@ echo -n "Starting Nginx:"
 systemctl enable nginx   &>>  ${LOGFILE}
 systemctl start nginx    &>>  ${LOGFILE}
 stat $?
+
+echo -n "Downloading the ${COMPONENT} component:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
+stat $? 
+
+
+echo -n "Clean up of ${COMPONENT} : "
+cd /usr/share/nginx/html    
+rm -rf *     &>>  ${LOGFILE}
+stat $?
+
+echo -n "Extracting ${COMPONENT} :"
+unzip /tmp/${COMPONENT}.zip     &>>  ${LOGFILE}
+stat $?
+
+echo -n "Sorting the ${COMPONENT} files :"
+mv ${COMPONENT}-main/* .
+mv static/* .
+rm -rf ${COMPONENT}-main README.md  &>>  ${LOGFILE}
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $? 
+
+echo -n "Updating the Backend Components in the reverse proxy file:"
+
+for component in catalogue ; do 
+    sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+done
+
+echo -n "Restarting ${COMPONENT}:"
+systemctl daemon-reload     &>>  ${LOGFILE}
+systemctl restart nginx     &>>  ${LOGFILE}
+stat $?
+
+echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
