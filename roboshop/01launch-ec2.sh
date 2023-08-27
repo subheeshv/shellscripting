255db2af8a6fcf24f612b0ae43c563fe8c2f2742
subhe@DESKTOP-OKROP93 <subheeshv@gmail.com>
2023-08-27 09:42:07 +0530

    Auto-push

--- roboshop/01launch-ec2.sh
+++ roboshop/01launch-ec2.sh
@@ -11,7 +11,7 @@ fi
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
-SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=b55-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
+SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=learn_test_group | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
 
 create_ec2() {
 
