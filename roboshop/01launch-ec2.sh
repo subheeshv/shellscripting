e5155815582f968948d758ee318b2a551b4ac246
subhe@DESKTOP-OKROP93 <subheeshv@gmail.com>
2023-09-01 08:26:52 +0530

    Auto-push

--- roboshop/01launch-ec2.sh
+++ roboshop/01launch-ec2.sh
@@ -1,7 +1,7 @@
 COMPONENT=$1 
 ENV=$2
 HOSTEDZONEID="Z05257022TR9OLEZRO8JM"
-INSTANCE_TYPE="t3.micro"
+INSTANCE_TYPE="t2.micro"
  
 if [ -z $1 ] || [ -z $2 ]  ; then 
     echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
@@ -10,7 +10,7 @@ if [ -z $1 ] || [ -z $2 ]  ; then
 fi 
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
-AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=mydevops_labimage_centos7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
 SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=learn_test_group | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
 
 create_ec2() {
